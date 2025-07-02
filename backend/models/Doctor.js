394162fa67381c5103a2b2051f512f46715937a2
kabir <k@example.com>
2025-07-02 20:45:21 +0500

    project plus readme file

--- backend/models/Doctor.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const DoctorSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  specialization: { type: String, default: 'General' }
-});
-
-module.exports = mongoose.model('Doctor', DoctorSchema);
